5fb565babf5a9940f1eaa3b665b82ab1eedfdfc2
Gilnei Freitas <118475451+GilneiAlves@users.noreply.github.com>
2025-10-08 12:08:27 -0300

    Valores padrão input

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ Preencha os campos ao lado para personalizar sua simulação.
 # --- Entradas do usuário ---
 st.sidebar.header("Parâmetros de Entrada CLT")
 
-salario_clt = st.sidebar.number_input("Salário bruto CLT (R$)", 0.0, 100000.0, 11100.0, step=500.0)
-alimentacao = st.sidebar.number_input("Benefício alimentação (R$)", 0.0, 5000.0, 1100.0, step=100.0)
+salario_clt = st.sidebar.number_input("Salário bruto CLT (R$)", 0.0, 100000.0, 10000.0, step=100.0)
+alimentacao = st.sidebar.number_input("Benefício alimentação (R$)", 0.0, 5000.0, 1000.0, step=100.0)
 plano_saude = st.sidebar.number_input("Plano de saúde (R$)", 0.0, 2000.0, 200.0, step=50.0)
 num_dependentes = st.sidebar.number_input("Número de dependentes", 0, 20, 0, step=1)
 
